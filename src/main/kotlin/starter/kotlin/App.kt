fbea9ada0aeb38cb8a5f05541cf48f316ac65ea6
Ginjiro Ueta <ginjiro.mmj@gmail.com>
2019-09-11 01:43:58 +0900

    inital commit

---
+++ src/main/kotlin/starter/kotlin/App.kt
@@ -0,0 +1,21 @@
+package starter.kotlin
+
+import org.jooby.Jooby.*
+import org.jooby.Kooby
+
+/**
+ * Gradle Kotlin stater project.
+ */
+class App : Kooby({
+    get {
+        val name = param("name").value("Kotlin")
+        "Hello $name!"
+    }
+})
+
+/**
+ * Run application:
+ */
+fun main(args: Array<String>) {
+    run(::App, args)
+}
